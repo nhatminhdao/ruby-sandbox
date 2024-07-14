add452ab98e6f87fb71984ac093bda0064568fe0
nhatminhdao <nhatminhdao@gmail.com>
2024-07-14 00:21:10 +0000

    edit

--- howdy.rb
+++ howdy.rb
@@ -2,6 +2,14 @@
 # my_string = "Hello, world!"
 # pp my_string
 
-require "./goodbye.rb"
-pp "table".pluralize
-pp 1.ordinalize
+# require "./goodbye.rb"
+require "active_support/all"
+a = "table".pluralize
+pp a
+# pp 1.ordinalize
+
+# pp "What's your name?"
+
+# their_name = gets.chomp
+
+# puts "Hello, pretty #{their_name}!"
